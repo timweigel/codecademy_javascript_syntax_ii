0d950eff8903f8663ee1ce8347e8a73c355cc5d5
Tim Weigel <timweigel@gmail.com>
2021-01-29 21:49:37 -0600

    Create greetAliens.js
    Adding my solution to the greetAliens() challenge in the Code Challenges: Intermediate Javascript lesson.

---
+++ greetAliens.js
@@ -0,0 +1,14 @@
+// Write your code here:
+function greetAliens(alien) {
+	const arrayLength = alien.length;
+	for (i = 0; i < arrayLength; i++) {
+		console.log(`Oh powerful ${alien[i]}, we humans offer our unconditional surrender!`);
+  }
+}
+
+// When you're ready to test your code, uncomment the below and run:
+///*
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
+//*/
